38fd8915928df409ca92d0f7fc6af2baeb527f3b
dkask <dkaskgkdua@gmail.com>
2023-03-06 00:55:02 +0900

    msa spring cloud eureka user-service - 인증 인가 개발중 - authenticationFilter 구현

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -5,14 +5,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("182.228.218.36") // IP 제한
+                .and()
+                .addFilter(getAuthentionFilter());
         http.headers().frameOptions().disable();
 
+    }
+
+    private AuthenticationFilter getAuthentionFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
