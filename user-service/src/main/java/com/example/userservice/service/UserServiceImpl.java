36dc5bd5698e69a96f5f687242bb9e7b56df58d0
dkask <dkaskgkdua@gmail.com>
2023-04-15 22:59:58 +0900

    user-service feign client 적용 - order service 사용 케이스 적용 - 로깅 설정 - exception 처리(404인데 최종적으로 클라이언트에는 500으로 떨어지는 문제)

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.userservice.service;
 
+import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,13 +26,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final Environment env;
-    private final RestTemplate restTemplate;
+    private final OrderServiceClient orderServiceClient;
+//    private final RestTemplate restTemplate;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -68,11 +73,20 @@ public class UserServiceImpl implements UserService {
 
 //        List<ResponseOrder> orders = new ArrayList<>();
         /* Using as Resttemplate*/
-        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
-        ResponseEntity<List<ResponseOrder>> orderListResponse = restTemplate.exchange(orderUrl, HttpMethod.GET, null,
-                new ParameterizedTypeReference<List<ResponseOrder>>() {
-                });
-        List<ResponseOrder> orders = orderListResponse.getBody();
+//        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+//        ResponseEntity<List<ResponseOrder>> orderListResponse = restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+//                new ParameterizedTypeReference<List<ResponseOrder>>() {
+//                });
+//        List<ResponseOrder> orders = orderListResponse.getBody();
+
+        /* Using as feign client */
+        List<ResponseOrder> orders = null;
+        try {
+            orders = orderServiceClient.getOrders(userId);
+        } catch(FeignException ex) {
+            log.error(ex.getMessage());
+        }
+
         userDto.setOrders(orders);
 
         return userDto;
